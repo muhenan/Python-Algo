aa7557d2f9c5a146a937d67eec7e4588b35260b4
Henan Mu <1149423577@qq.com>
2026-03-30 23:51:13 -0400

    lint

--- test/mb.py
+++ test/mb.py
@@ -31,7 +31,7 @@ def dfs(operators, m, exclusions, current_ans, ans):
             ans_once.append(v)
         ans.append(ans_once)
     for i in range(len(operators)):
-        if len(current_ans) == 0 or (current_ans[-1] not in exclusions or operators[i] not in exclusions[current_ans[-1]]):
+        if (len(current_ans) == 0 or current_ans[-1] not in exclusions or operators[i] not in exclusions[current_ans[-1]]):
             current_ans.append(operators[i])
             dfs(operators, m, exclusions, current_ans, ans)
             current_ans.pop()
